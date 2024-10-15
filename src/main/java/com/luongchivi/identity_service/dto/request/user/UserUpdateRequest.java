290f7d49a7a63197e82dfd3ed8af260d20aca71a
Luong Chi Vi <chivi060399@gmail.com>
2024-10-15 13:37:27 +0700

    Init project

---
+++ src/main/java/com/luongchivi/identity_service/dto/request/user/UserUpdateRequest.java
@@ -0,0 +1,20 @@
+package com.luongchivi.identity_service.dto.request.user;
+
+import java.time.LocalDate;
+
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class UserUpdateRequest {
+    String username;
+    String password;
+    String firstName;
+    String lastName;
+    LocalDate dateOfBirth;
+}
